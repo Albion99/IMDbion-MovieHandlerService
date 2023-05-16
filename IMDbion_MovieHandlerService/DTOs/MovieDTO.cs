a6bd02265d799c6c4a923da94ea3034130696379
Albion <461340@student.fontys.nl>
2023-05-17 00:35:31 +0200

    Added Id in moviedto

--- IMDbion_MovieHandlerService/DTOs/MovieDTO.cs
+++ IMDbion_MovieHandlerService/DTOs/MovieDTO.cs
@@ -7,6 +7,7 @@ namespace IMDbion_MovieHandlerService.DTOs
 {
     public class MovieDTO
     {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Genre { get; set; }
