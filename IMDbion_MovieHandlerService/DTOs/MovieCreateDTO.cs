16b239094c4ec5281d7e55452f19f7b0a8ce0a4d
Albion <461340@student.fontys.nl>
2023-06-13 16:45:33 +0200

    added regex validation

--- IMDbion_MovieHandlerService/DTOs/MovieCreateDTO.cs
+++ IMDbion_MovieHandlerService/DTOs/MovieCreateDTO.cs
@@ -1,14 +1,29 @@
-﻿namespace IMDbion_MovieHandlerService.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMDbion_MovieHandlerService.DTOs
 {
     public class MovieCreateDTO
     {
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Only alphabetic characters are allowed.")]
         public string Title { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Only alphabetic characters are allowed.")]
         public string Description { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Only alphabetic characters are allowed.")]
         public string Genre { get; set; }
+
+        [RegularExpression(@"^\d+$", ErrorMessage = "Only numeric characters are allowed.")]
         public int Length { get; set; }
+
         public DateTime PublicationDate { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Only alphabetic characters are allowed.")]
         public string CountryOfOrigin { get; set; }
+
         public List<Guid> ActorIds { get; set; }
+
+        [RegularExpression(@"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\.mp4$", ErrorMessage = "Invalid video path format.")]
         public string VideoPath { get; set; }
     }
 }
